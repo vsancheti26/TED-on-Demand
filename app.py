141734688838d87eb16ee67428dc25bea511c4d6
MierafM <mierafmulat@gmail.com>
2022-04-28 13:35:40 -0500

    adding result and loading pages

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 
 app = Flask(__name__)
 if __name__ == '__main__':
@@ -9,3 +9,13 @@ if __name__ == '__main__':
 @app.route("/")  
 def home():
 	return render_template("index.html") 
+
+@app.route('/', methods = ['POST'])
+def form_post():
+	user_input = request.form['promptInput']
+	print(user_input)
+	return render_template("result.html", prompt=user_input)
+
+@app.route("/loading")  
+def loading():
+	return render_template("loading.html") 
