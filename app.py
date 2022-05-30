c93105b1b8b40e5779fb3779992458b8de21fd43
MierafM <mierafmulat@gmail.com>
2022-05-29 19:10:05 -0500

    renaming vids works

--- app.py
+++ app.py
@@ -47,31 +47,36 @@ def form_post():
 			type='photo'
 			)
 		google_Crawler = GoogleImageCrawler(storage = {'root_dir': 'images'})
-		google_Crawler.crawl(filters = filters,keyword = sentence, max_num = 10)
-		"""
-		trying to fix image naming issue
+		google_Crawler.crawl(filters = filters,keyword = sentence, max_num = 5)
+		
+		# trying to fix image naming issue
+		# select newly added imgs
 		file_path = 'C:/Users/miera/Desktop/cs338/TED-on-Demand/images/0000'
 		final_file_paths = []
-		for i in range(1,11):
+		for i in range(1,6):
 			if i!=10:
 				final_file_path = file_path + '0' + str(i) + '.jpg'
 			else:
 				final_file_path = file_path + str(i) + '.jpg'
 			final_file_paths.append(final_file_path)
+		#for new imgs rename them 
 		for i in range(len(final_file_paths)):
 			try:
-				new_name = final_file_paths[i][:-5] + str(count) + '.jpg'
+				new_name = final_file_paths[i][:-5] + str(count) +'_new_'+ '.jpg'
 				print('NEW NAME IS: ',new_name)
 				count+=1
 				os.rename(final_file_paths[i], new_name)
+				
 
 			except:
-				new_name = final_file_paths[i][:-5] + str(count) + '.png'
+				new_name = final_file_paths[i][:-5] + str(count)+'_new_' + '.png'
 				print('png old name',final_file_paths[i] )
 				print('png NEW NAME IS: ',new_name)
 				count+=1
 				os.rename(final_file_paths[i][:-3]+'png', new_name)
-		"""	
+				
+
+		
 
 	path = 'images'
 
@@ -85,7 +90,8 @@ def form_post():
 			filenames.append(os.path.join(path, filename))
 
 	filenames.sort() 
-
+	for filename in filenames:
+		print('filename',filename, imageio.imread(filename))
 	images = list(map(lambda filename: Image.fromarray(imageio.imread(filename)).resize((512, 512)), filenames))
 
 	for i in range(len(images)):
